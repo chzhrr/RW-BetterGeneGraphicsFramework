53316cc9c5067dc7b1118fa6a94d36cc74c20fe9
chzhrr <chzhrr@gmail.com>
2024-04-04 21:38:53 -0700

    Add PawnRenderNode_GeneHeadAttachment for head attachments.

---
+++ Source/PawnRenderNode_GeneHeadAttachment.cs
@@ -0,0 +1,20 @@
+﻿using Verse;
+
+namespace BetterGeneGraphicsFramework
+{
+    /// <summary>
+    /// Scale head attachments with head texture size.
+    /// </summary>
+    public class PawnRenderNode_GeneHeadAttachment : PawnRenderNode_GeneAttachment
+    {
+        public PawnRenderNode_GeneHeadAttachment(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree) 
+            : base(pawn, props, tree)
+        {
+        }
+
+        public override GraphicMeshSet MeshSetFor(Pawn pawn)
+        {
+            return HumanlikeMeshPoolUtility.GetHumanlikeHairSetForPawn(pawn);
+        }
+    }
+}
